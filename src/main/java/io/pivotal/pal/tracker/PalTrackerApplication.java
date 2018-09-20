f7974f5b6f2f994733409673ffa0b5b643918263
=utkarshemc <=utkarsh.bhardwaj@emc.com>
2018-09-20 15:10:29 +0530

    Jdbc Commit

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
@@ -16,11 +18,17 @@ public class PalTrackerApplication {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
-    @Bean
+   /* @Bean
     TimeEntryRepository timeEntryRepository()
     {
         return new InMemoryTimeEntryRepository();
+    }*/
+
+    @Bean
+    TimeEntryRepository timeEntryRepository(DataSource dataSource) {
+        return new JdbcTimeEntryRepository(dataSource);
     }
+
 
     @Bean
     public ObjectMapper jsonObjectMapper() {
